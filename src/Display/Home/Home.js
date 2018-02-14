a87b2d31b196e3a87d7470779c1b45265b624121
Mark Gable <mgable43@gmail.com>
2018-02-13 17:04:02 -0800

    working

--- src/Display/Home/Home.js
+++ src/Display/Home/Home.js
@@ -6,20 +6,20 @@ class Home extends React.Component{
 		return (
 			<div className="home">
 				 <div className="container">
-				 	<div className="row first">
-                		<div className="col-sm-12">
-                			<h1>Home</h1>
-                			<p>this shoudl be one line line</p>
-                		</div>
-                	</div>
+					<div className="row first">
+						<div className="col-sm-12">
+							<h1>Home</h1>
+							<p>this shoudl be one line line</p>
+						</div>
+					</div>
 
-                	<div className="row second">
-                		<div className="col-sm-12">
-                			<h1>Home</h1>
-                			<p>this shoudl be one line line</p>
-                		</div>
-                	</div>
-                </div>
+					<div className="row second">
+						<div className="col-sm-12">
+							<h1>Home</h1>
+							<p>this shoudl be one line line</p>
+						</div>
+					</div>
+				</div>
 			</div>
 		)
 	}
