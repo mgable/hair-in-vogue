05976329bf6b43668be4bab22b7091c577f73d28
Mark Gable <mgable43@gmail.com>
2018-02-22 16:27:50 -0800

    mobile styling

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -28,9 +28,9 @@ class Nav extends React.Component{
 
 	render() {
 		return (
-			<nav className="navbar navbar-expand-lg hiv-bg-light">
+			<nav className="navbar navbar-expand-lg navbar-light hiv-bg-light">
 				<div className="container">
-					<a className="navbar-brand" href="/"><img src="https://dummyimage.com/290x40/EBE1DF/000.jpg&text=HAIR-IN-VOGUE-LOGO" width="290" height="40" alt="hair in vogue logo"/></a>
+					<a className="navbar-brand" href="/"><img className="mobile" src="https://dummyimage.com/265x35/EBE1DF/000.jpg&text=HAIR-IN-VOGUE-LOGO" width="265" height="35" alt="hair in vogue logo"/><img className="desktop" src="https://dummyimage.com/290x40/EBE1DF/000.jpg&text=HAIR-IN-VOGUE-LOGO" width="290" height="40" alt="hair in vogue logo"/></a>
 					<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 						<span className="navbar-toggler-icon" />
 					</button>
